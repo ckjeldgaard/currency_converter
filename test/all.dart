7222d548ecd00e783beb335c6a53859f9becff76
Christoffer Kjeldgaard Petersen <christoffer@sappy.dk>
2016-12-25 13:39:42 +0100

    Added currency_converter_view_tests to all.dart tests.

--- test/all.dart
+++ test/all.dart
@@ -1,5 +1,7 @@
 import 'sample_test.dart' as sample_tests;
+import 'currency_converter_view_test.dart' as currency_converter_view_tests;
 
 void main() {
+  currency_converter_view_tests.main();
   sample_tests.main();
 }
