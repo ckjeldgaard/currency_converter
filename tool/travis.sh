dad757780eda078aa09da0fba339b4ea96cb88b4
Christoffer Kjeldgaard Petersen <christoffer@sappy.dk>
2017-01-03 15:25:06 +0100

    Explicitly telling Travis which tests to run on content-shell

--- tool/travis.sh
+++ tool/travis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 pub get --packages-dir
-pub run test
-pub run test -p content-shell
+pub run test test/unit/
+pub run test -p content-shell test/browser/
 pub run dart_codecov_generator --no-html --verbose test/unit/
 bash <(curl -s https://codecov.io/bash)
