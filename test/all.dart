e445b658be429d6760552f461d7407c7ebd5ad19
Christoffer Kjeldgaard Petersen <christoffer@sappy.dk>
2017-01-03 14:34:06 +0100

    Removed unused sample_test

--- test/all.dart
+++ test/all.dart
@@ -1,7 +1,5 @@
-import 'sample_test.dart' as sample_tests;
 import 'currency_converter_presenter_test.dart' as currency_converter_presenter_tests;
 
 void main() {
-  sample_tests.main();
   currency_converter_presenter_tests.main();
 }
