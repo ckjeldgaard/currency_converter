2584b634f241d640f7570f99179001f8d0dad976
Christoffer Kjeldgaard Petersen <christoffer@sappy.dk>
2017-01-09 15:37:04 +0100

    Moved from dart_codecov_generator to dart_coveralls

---
+++ test/test_all.dart
@@ -0,0 +1,9 @@
+import 'unit/currency_converter_presenter_test.dart' as currency_converter_presenter_test;
+import 'unit/default_currency_test.dart' as default_currency_test;
+
+import 'package:test/test.dart';
+
+void main() {
+  group('currency_converter_presenter', currency_converter_presenter_test.main);
+  group('default_currency', default_currency_test.main);
+}
--- tool/travis.sh
+++ tool/travis.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
 pub get --packages-dir
+
+# Fast fail the script on failures.
+set -e
+
+# Run the tests.
 pub run test test/unit/
 pub run test -p content-shell test/browser/
-pub run dart_codecov_generator --no-html --verbose test/unit/
-bash <(curl -s https://codecov.io/bash)
+
+# If the COVERALLS_TOKEN token is set on travis
+# Install dart_coveralls
+# Rerun tests with coverage and send to coveralls
+if [ "$COVERALLS_TOKEN" ]; then
+  pub global activate dart_coveralls
+  pub global run dart_coveralls report \
+    --token $COVERALLS_TOKEN \
+    --retry 2 \
+    --exclude-test-files \
+    test/test_all.dart
+fi
