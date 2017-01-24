cea174fa70a1ec6196e6f84d5b33bd96885eba79
Christoffer Kjeldgaard Petersen <christoffer@sappy.dk>
2017-01-24 11:24:12 +0100

    Initial setup of service worker.

--- web/main.dart
+++ web/main.dart
@@ -1,10 +1,28 @@
 import 'app/converter/currency_converter_presenter.dart';
 import 'app/converter/currency_converter_view.dart';
+import 'app/data/database_provider.dart';
 import 'app/data/fixer_repository.dart';
+import 'app/model/currency.dart';
+import 'app/model/stored_currencies.dart';
+import 'app/service_worker/service_worker_manager.dart';
 import 'app/utils/sidedrawer/overlay_invoker.dart';
 import 'app/utils/sidedrawer/sidedrawer.dart';
 
+class CurrencyConverterApp {
+
+  final ServiceWorkerManager _serviceWorker = new ServiceWorkerManager();
+
+  init() async {
+    await _serviceWorker.registerServiceWorker();
+    new SideDrawer(new OverlayInvoker());
+    new CurrencyConverterView(new CurrencyConverterPresenter(new FixerRepository()));
+  }
+}
+
 void main() {
-  new SideDrawer(new OverlayInvoker());
-  new CurrencyConverterView(new CurrencyConverterPresenter(new FixerRepository()));
+  new CurrencyConverterApp().init();
+
+  /* for (Currency c in new StoredCurrencies(new DatabaseProvider().db)) {
+    print("currency = " + c.code);
+  } */
 }
