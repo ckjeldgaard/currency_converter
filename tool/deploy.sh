3234c27a5e54583063e250ce35ac8829863a890d
Christoffer Kjeldgaard Petersen <christoffer@sappy.dk>
2017-02-03 14:26:16 +0100

    Added Travis deploy script to GitHub pages.

---
+++ tool/deploy.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -o errexit -o nounset
+
+if [ "$TRAVIS_BRANCH" != "master" ]
+then
+  echo "This commit was made against the $TRAVIS_BRANCH and not the master! No deploy!"
+  exit 0
+fi
+
+rev=$(git rev-parse --short HEAD)
+
+cd build
+
+git init
+git config user.name "Travis-CI"
+git config user.email "travis@sappy.dk"
+
+git remote add upstream "https://$GH_TOKEN@github.com/ckjeldgaard/currency_converter.git"
+git fetch upstream
+git reset upstream/gh-pages
+
+echo "ckjeldgaard.github.io/currency_converter/" > CNAME
+
+touch .
+
+git add -A .
+git commit -m "rebuild pages at ${rev}"
+git push -q upstream HEAD:gh-pages
